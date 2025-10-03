77ebc14dbcc12c9548ae3f32f59f213b38e6f4c4
edeldev <edeldev25@gmail.com>
2025-10-03 17:26:06 -0600

    chore: change media query

--- components/HomePage/components/ButtonEnterMobile.tsx
+++ components/HomePage/components/ButtonEnterMobile.tsx
@@ -6,7 +6,7 @@ export const ButtonEnterMobile = ({ setShowForm }: IButtonEnterMobile) => {
       className="mt-4 uppercase text-sm px-7 py-3 bg-primary text-black rounded transition-all duration-300
     hover:[filter:brightness(1.1)]
     hover:[transform:translateY(-1px)]
-    hover:[box-shadow:0_8px_25px_rgba(0,255,156,0.2)] cursor-pointer block md:hidden"
+    hover:[box-shadow:0_8px_25px_rgba(0,255,156,0.2)] cursor-pointer block xl:hidden"
       onClick={() => setShowForm(true)}
     >
       Enter
